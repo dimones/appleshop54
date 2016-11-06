32fc5b66e4b895db5c9139a296e28c2dd1a70bc9
Дмитрий Богомолов <Dimones951@yandex.ru>
2016-11-06 17:21:26 +0700

    Test

--- appleshop54.py
+++ appleshop54.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('f32d73dc8d1d.html')
+def f32d73dc8d1d():
+    return 'a1a24e65907a'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=80, debug=True)
