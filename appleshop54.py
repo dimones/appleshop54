e7a53bb19682302d2fd532ce25539e533da922b4
Дмитрий Богомолов <Dimones951@yandex.ru>
2016-11-20 11:49:24 +0700

    admin panel

--- appleshop54.py
+++ appleshop54.py
@@ -1,9 +1,21 @@
 from flask import *
-
+import os,pymysql,sys,base64
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
-from flask_admin import Admin
-admin = Admin(app, name='appleshop54', template_mode='bootstrap3')
 
+UPLOAD_FOLDER = '/Users/dimones-dev/appleshop54'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+def get_conn():
+    return pymysql.connect(host='127.0.0.1',
+                             user='root',
+                             password='jmXQF97J',
+                             db='appleshop54',
+                             charset='utf8')
 
 @app.route('/')
 def main():
@@ -11,8 +23,128 @@ def main():
 
 @app.route('/contacts')
 def contacts():
-    return render_template('index.html',body=render_template('page.html'))
+    return render_template('index.html',body=render_template('page.html',body=render_template('contacts.html')))
 
+@app.route('/ad')
+def admin():
+    return render_template('admin.html')
+@app.route('/ad/new')
+def admin_new():
+    return render_template('admin_new_product.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'tmp'))
+            buffer = None
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], 'tmp'),'rb') as file:
+                buffer = file.read()
+                file.close()
+            connection = get_conn()
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO product_images(image,product_id,is_main,extension) VALUES ('%s',%s,%s,'image/%s')" % (base64.b64encode(buffer).decode('utf8'),-1,0,filename.rsplit('.', 1)[1]))
+                    connection.commit()
+                    connection.close()
+                    return json.dumps({'imgId': cursor.lastrowid})
+            except Exception as e:
+                print(str(e), file=sys.stderr)
+                return json.dumps({'succeed': False, "error": str(e)})
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'good'
+
+
+@app.route('/ad/img/setProductID', methods=['GET'])
+def img_setProductID():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+
+            cursor.execute(
+                "UPDATE product_images SET is_main = 1 WHERE id = %s" % (request.args.get('product_id'),request.args.get('image_id')))
+            connection.commit()
+            connection.close()
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+
+@app.route('/ad/img/remove', methods=['GET'])
+def img_remove():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "DELETE FROM product_images WHERE id = %s" % (request.args.get('image_id')))
+            connection.commit()
+            connection.close()
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+
+@app.route('/ad/img/setMain', methods=['GET'])
+def img_setMain():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('UPDATE product_images SET is_main = 0 WHERE is_main = 1')
+            connection.commit()
+            cursor.execute(
+                "UPDATE product_images SET is_main = 1 WHERE id = %s" % (request.args.get('image_id')))
+            connection.commit()
+            connection.close()
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+
+
+@app.route('/ad/img/all')
+def img_all():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT id,image,extension,is_main FROM product_images WHERE product_id = -1")
+            images = []
+            for data in cursor.fetchall():
+                images.append({'id':data[0] ,'image':('data:image/%s;base64,%s' %(data[2],data[1])), 'is_main' :bool(data[3]) })
+            connection.close()
+            return json.dumps(images)
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+
+@app.route('/ad/img')
+def img():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT image,extension FROM product_images WHERE id = %s" % (request.args.get('image_id')))
+            if cursor.rowcount > 0:
+                data = cursor.fetchone()
+                connection.close()
+                return 'data:image/%s;base64,%s' %(data[1],data[0])
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
 
 @app.route('/f32d73dc8d1d.html')
 def f32d73dc8d1d():
