d3b065aeec50bd7bb41e49ac815e20a8045a0788
Дмитрий Богомолов <Dimones951@yandex.ru>
2016-11-20 15:55:03 +0700

    Admin panel tesing

--- appleshop54.py
+++ appleshop54.py
@@ -33,7 +33,19 @@ def contacts():
 
 @app.route('/ad')
 def admin():
-    return render_template('admin.html')
+    connection = get_conn_1()
+    data = None
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT pr.id,pr.type_product , pr. NAME , pr.price ,"
+                           "( SELECT image FROM product_images WHERE product_id = pr.id AND is_main = 1) image ,"
+                           "( SELECT extension FROM product_images WHERE product_id = pr.id AND is_main = 1) extension FROM products pr")
+            connection.close()
+            data = cursor.fetchall()
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+    return render_template('admin.html',product_list=render_template('product_list.html',products=data))
 @app.route('/ad/new')
 def admin_new():
     return render_template('admin_new_product.html')
@@ -72,7 +84,26 @@ def upload_file():
                 return json.dumps({'succeed': False, "error": str(e)})
             # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return 'good'
-
+@app.route('/ad/product/get_by_type')
+def product_getByType():
+    connection = get_conn_1()
+    data = None
+    try:
+        with connection.cursor() as cursor:
+            if request.args.get('type') is None:
+                cursor.execute("SELECT pr.id,pr.type_product , pr. NAME , pr.price ,"
+                               "( SELECT image FROM product_images WHERE product_id = pr.id AND is_main = 1) image ,"
+                               "( SELECT extension FROM product_images WHERE product_id = pr.id AND is_main = 1) extension FROM products pr")
+            else:
+                cursor.execute("SELECT pr.id,pr.type_product , pr. NAME , pr.price ,"
+                               "( SELECT image FROM product_images WHERE product_id = pr.id AND is_main = 1) image ,"
+                               "( SELECT extension FROM product_images WHERE product_id = pr.id AND is_main = 1) extension FROM products pr WHERE pr.type_product = %s" % (request.args.get('type')))
+            connection.close()
+            data = cursor.fetchall()
+            return render_template('product_list.html',products=data)
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
 @app.route('/ad/product/get_types', methods=['GET'])
 def product_getTypes():
     connection = get_conn_1()
@@ -91,14 +122,45 @@ def product_add():
     connection = get_conn()
     try:
         with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO products()")
-
+            cursor.execute("INSERT INTO products(type_product,name,price,description) VALUES(%s,'%s',%s,'%s')"
+                           %(request.args.get('type_product'),request.args.get('name'),
+                             request.args.get('price'),request.args.get('description')))
+            connection.commit()
             connection.close()
-            return json.dumps({'productId': cursor.fetchone()[0]})
+            return json.dumps({'succeed':True})
     except Exception as e:
         print(str(e), file=sys.stderr)
         return json.dumps({'succeed': False, "error": str(e)})
 
+@app.route('/ad/product/remove',methods= ['GET'])
+def product_remove():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            print(request.args.get('product_id'))
+            cursor.execute("DELETE FROM products WHERE id = %s" % request.args.get('product_id'))
+            connection.commit()
+            connection.close()
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
+@app.route('/ad/product/set_spec',methods=['POST'])
+def product_setSpec():
+    connection = get_conn()
+    try:
+        with connection.cursor() as cursor:
+            productSpecs = json.loads(request.form['specs'])
+            productId = request.form['product_id']
+            for spec in productSpecs:
+                cursor.execute("INSERT INTO product_specs(specName,specValue,product_id) VALUES('%s','%s',%s)"
+                               % (spec['specName'],spec['specValue'],productId))
+            connection.commit()
+            connection.close()
+            return json.dumps({'succeed': True})
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return json.dumps({'succeed': False, "error": str(e)})
 @app.route('/ad/product/next', methods=['GET'])
 def product_next():
     connection = get_conn()
@@ -119,9 +181,8 @@ def img_setProductID():
     connection = get_conn()
     try:
         with connection.cursor() as cursor:
-
             cursor.execute(
-                "UPDATE product_images SET is_main = 1 WHERE id = %s" % (request.args.get('product_id'),request.args.get('image_id')))
+                "UPDATE product_images SET product_id = %s WHERE id = %s" % (request.args.get('product_id'),request.args.get('image_id')))
             connection.commit()
             connection.close()
             return json.dumps({'succeed': True})
@@ -148,10 +209,10 @@ def img_setMain():
     connection = get_conn()
     try:
         with connection.cursor() as cursor:
-            cursor.execute('UPDATE product_images SET is_main = 0 WHERE is_main = 1')
+            cursor.execute('UPDATE product_images SET is_main = 0 WHERE  product_id = -1 AND is_main = 1')
             connection.commit()
             cursor.execute(
-                "UPDATE product_images SET is_main = 1 WHERE id = %s" % (request.args.get('image_id')))
+                "UPDATE product_images SET is_main = 1 WHERE  product_id = -1 AND id = %s" % (request.args.get('image_id')))
             connection.commit()
             connection.close()
             return json.dumps({'succeed': True})
